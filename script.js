096fd1e4fb167a4db65a7f8e3054bc1bca350dd1
BitBanana <bitbananaofficial@gmail.com>
2025-06-17 20:45:05 +0200

    step 2: Get microphone access

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const ctx = canvas.getContext("2d");
 
 const startButton = document.getElementById("startButton");
 
-startButton.addEventListener("click", () => {
-  setupMicrophone();
+startButton.addEventListener("click", async () => {
+  await setupMicrophone();
   startButton.textContent = "Stop Microphone";
   drawWaveform();
 });
 
-function setupMicrophone() {}
+async function setupMicrophone() {
+  // Prompt user for microphone permission
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+  // Set up an "Audio Graph", check WebAudioAPI for further info:
+  // https://developer.mozilla.org/en-US/docs/Web/API/AudioContext
+  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+  // Create input audio node
+  const microphone = audioContext.createMediaStreamSource(stream);
+  // Create analyser audio node
+  const analyser = audioContext.createAnalyser();
+  // Connect input node to analyser
+  microphone.connect(analyser);
+}
 
 function drawWaveform() {}
